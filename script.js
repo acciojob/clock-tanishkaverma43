c290c22d7b9336f77731c247eaa27441691cf871
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-04 19:33:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let timerdisplay = document.getElementId("timer");
+
+let currentDate = new Date();
+
+setInterval(
+	function{
+		currentDate = new Date();
+
+   //set the current time
+    timerdisplay.innerHTML = currentDate.toLocaleString();
+
+	},1000
+)
